256eac5aa52ed74fd88f78726cead5c3d6d4d34d
Pea <pea@misfist.com>
2015-04-04 00:04:45 -0400

    Added order by ASC.

--- wp-content/themes/thelearningcollective/functions.php
+++ wp-content/themes/thelearningcollective/functions.php
@@ -19,12 +19,16 @@ function tlc_custom_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'tlc_custom_scripts', 99 );
 
+/**
+ * Custom Functions
+ */
 
 // Change sort order for Resources and Projects
 
 function tlc_resources_projects_alpha_sort( $query ) {
     if ( $query->is_category( 'projects' ) || $query->is_category( 'resources' ) && $query->is_main_query()  ) {
         $query->set( 'orderby', 'title' );
+		$query->set( 'order', 'ASC' );
     } 
 }
 add_action( 'pre_get_posts', 'tlc_resources_projects_alpha_sort' );
