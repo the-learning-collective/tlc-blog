d045c06a5f308da342e4a7a3dfa898cddd30e02d
Pea <pea@misfist.com>
2015-04-04 00:01:01 -0400

    Removed custom archive. Added pre_get_posts action to sort posts categorized as Resources or Projects alphabetically.

--- wp-content/themes/thelearningcollective/functions.php
+++ wp-content/themes/thelearningcollective/functions.php
@@ -20,4 +20,13 @@ function tlc_custom_scripts() {
 add_action( 'wp_enqueue_scripts', 'tlc_custom_scripts', 99 );
 
 
+// Change sort order for Resources and Projects
+
+function tlc_resources_projects_alpha_sort( $query ) {
+    if ( $query->is_category( 'projects' ) || $query->is_category( 'resources' ) && $query->is_main_query()  ) {
+        $query->set( 'orderby', 'title' );
+    } 
+}
+add_action( 'pre_get_posts', 'tlc_resources_projects_alpha_sort' );
+
 ?>
